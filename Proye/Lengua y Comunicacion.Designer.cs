48e9736b5bd9e9013093006388f5a44ed59e5d47
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-24 08:14:25 -0600

    apartado grafico de espaniol

---
+++ Proye/Lengua y Comunicacion.Designer.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC02
+ * Date: 24/10/2024
+ * Time: 08:06 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Proye
+{
+	partial class Lengua_y_Comunicacion
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(198, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(183, 80);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Lengua y comunicacion\r\n";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			//this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(257, 162);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Hola";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// Lengua_y_Comunicacion
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(587, 415);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "Lengua_y_Comunicacion";
+			this.Text = "Lengua_y_Comunicacion";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Proye/Lengua y Comunicacion.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC02
+ * Date: 24/10/2024
+ * Time: 08:06 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Proye
+{
+	/// <summary>
+	/// Description of Lengua_y_Comunicacion.
+	/// </summary>
+	public partial class Lengua_y_Comunicacion : Form
+	{
+		public Lengua_y_Comunicacion()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		
+		
+	}
+}
