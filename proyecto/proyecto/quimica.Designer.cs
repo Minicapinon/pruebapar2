24e395675fb15d86f6c71c54ae4651f67ac680c4
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-10-31 15:23:57 +0100

    Se agrego la parte de Quimica

--- proyecto/proyecto/quimica.Designer.cs
+++ proyecto/proyecto/quimica.Designer.cs
@@ -36,12 +36,328 @@ namespace proyecto
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(quimica));
+			this.btnMen = new System.Windows.Forms.Button();
+			this.btnHomo1 = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.btnGaso = new System.Windows.Forms.Button();
+			this.btnAceYagu = new System.Windows.Forms.Button();
+			this.btnPerf = new System.Windows.Forms.Button();
+			this.label9 = new System.Windows.Forms.Label();
+			this.btnHetero2 = new System.Windows.Forms.Button();
+			this.btnHomo2 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.btnHetero1 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnMen
+			// 
+			this.btnMen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnMen.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnMen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnMen.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnMen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnMen.Location = new System.Drawing.Point(40, 600);
+			this.btnMen.Name = "btnMen";
+			this.btnMen.Size = new System.Drawing.Size(53, 27);
+			this.btnMen.TabIndex = 40;
+			this.btnMen.Text = "MENÚ";
+			this.btnMen.UseVisualStyleBackColor = false;
+			this.btnMen.Click += new System.EventHandler(this.BtnMenClick);
+			// 
+			// btnHomo1
+			// 
+			this.btnHomo1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnHomo1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnHomo1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnHomo1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnHomo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnHomo1.Location = new System.Drawing.Point(40, 410);
+			this.btnHomo1.Name = "btnHomo1";
+			this.btnHomo1.Size = new System.Drawing.Size(102, 34);
+			this.btnHomo1.TabIndex = 39;
+			this.btnHomo1.Text = "HOMOGÉNEA";
+			this.btnHomo1.UseVisualStyleBackColor = false;
+			this.btnHomo1.Click += new System.EventHandler(this.BtnHomo1Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(249, 162);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(173, 100);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 38;
+			this.pictureBox3.TabStop = false;
+			// 
+			// btnGaso
+			// 
+			this.btnGaso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnGaso.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnGaso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnGaso.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnGaso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnGaso.Location = new System.Drawing.Point(544, 547);
+			this.btnGaso.Name = "btnGaso";
+			this.btnGaso.Size = new System.Drawing.Size(102, 34);
+			this.btnGaso.TabIndex = 37;
+			this.btnGaso.Text = "GASOLINA";
+			this.btnGaso.UseVisualStyleBackColor = false;
+			this.btnGaso.Click += new System.EventHandler(this.BtnGasoClick);
+			// 
+			// btnAceYagu
+			// 
+			this.btnAceYagu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnAceYagu.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnAceYagu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAceYagu.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAceYagu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnAceYagu.Location = new System.Drawing.Point(544, 475);
+			this.btnAceYagu.Name = "btnAceYagu";
+			this.btnAceYagu.Size = new System.Drawing.Size(102, 45);
+			this.btnAceYagu.TabIndex = 36;
+			this.btnAceYagu.Text = "ACEITE Y AGUA";
+			this.btnAceYagu.UseVisualStyleBackColor = false;
+			this.btnAceYagu.Click += new System.EventHandler(this.BtnAceYaguClick);
+			// 
+			// btnPerf
+			// 
+			this.btnPerf.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnPerf.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnPerf.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnPerf.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnPerf.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnPerf.Location = new System.Drawing.Point(544, 410);
+			this.btnPerf.Name = "btnPerf";
+			this.btnPerf.Size = new System.Drawing.Size(102, 34);
+			this.btnPerf.TabIndex = 35;
+			this.btnPerf.Text = "PERFUME";
+			this.btnPerf.UseVisualStyleBackColor = false;
+			this.btnPerf.Click += new System.EventHandler(this.BtnPerfClick);
+			// 
+			// label9
+			// 
+			this.label9.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(509, 337);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(163, 36);
+			this.label9.TabIndex = 34;
+			this.label9.Text = "¿Cúal de estas mezclas NO es homogenea?";
+			// 
+			// btnHetero2
+			// 
+			this.btnHetero2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnHetero2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnHetero2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnHetero2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnHetero2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnHetero2.Location = new System.Drawing.Point(290, 475);
+			this.btnHetero2.Name = "btnHetero2";
+			this.btnHetero2.Size = new System.Drawing.Size(102, 34);
+			this.btnHetero2.TabIndex = 33;
+			this.btnHetero2.Text = "HETEROGÉNEA";
+			this.btnHetero2.UseVisualStyleBackColor = false;
+			this.btnHetero2.Click += new System.EventHandler(this.BtnHetero2Click);
+			// 
+			// btnHomo2
+			// 
+			this.btnHomo2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnHomo2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnHomo2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnHomo2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnHomo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnHomo2.Location = new System.Drawing.Point(290, 410);
+			this.btnHomo2.Name = "btnHomo2";
+			this.btnHomo2.Size = new System.Drawing.Size(102, 34);
+			this.btnHomo2.TabIndex = 32;
+			this.btnHomo2.Text = "HOMOGÉNEA";
+			this.btnHomo2.UseVisualStyleBackColor = false;
+			this.btnHomo2.Click += new System.EventHandler(this.BtnHomo2Click);
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(249, 337);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(196, 36);
+			this.label8.TabIndex = 31;
+			this.label8.Text = "¿Qué tipo de mezcla se crea al mezclar agua y arena?";
+			// 
+			// btnHetero1
+			// 
+			this.btnHetero1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(143)))), ((int)(((byte)(161)))));
+			this.btnHetero1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnHetero1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnHetero1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnHetero1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(35)))), ((int)(((byte)(79)))));
+			this.btnHetero1.Location = new System.Drawing.Point(40, 475);
+			this.btnHetero1.Name = "btnHetero1";
+			this.btnHetero1.Size = new System.Drawing.Size(102, 34);
+			this.btnHetero1.TabIndex = 30;
+			this.btnHetero1.Text = "HETEROGÉNEA";
+			this.btnHetero1.UseVisualStyleBackColor = false;
+			this.btnHetero1.Click += new System.EventHandler(this.BtnHetero1Click);
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(12, 337);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(177, 36);
+			this.label7.TabIndex = 29;
+			this.label7.Text = "¿Qué tipo de mezcla es el café  con leche?";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(406, 357);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(165, 270);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 28;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(110, 357);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(165, 270);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 27;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.Color.Black;
+			this.label6.Location = new System.Drawing.Point(29, 136);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(204, 100);
+			this.label6.TabIndex = 26;
+			this.label6.Text = "Son aquellas en las que los componentes son distintos y se pueden observar. La me" +
+			"zcla no es uniforme y cada componente mantiene sus propiedades.";
+			// 
+			// label5
+			// 
+			this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.label5.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.Black;
+			this.label5.Location = new System.Drawing.Point(452, 136);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(220, 100);
+			this.label5.TabIndex = 25;
+			this.label5.Text = "Son aquellas en las que los componentes están completamente mezclados y no se pue" +
+			"den distinguir a simple vista. La mezcla es uniforme en toda su extensión.";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Lucida Sans Typewriter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.Black;
+			this.label4.Location = new System.Drawing.Point(52, 86);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(137, 21);
+			this.label4.TabIndex = 24;
+			this.label4.Text = "HETEROGÉNEA";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Lucida Sans Typewriter", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.Black;
+			this.label3.Location = new System.Drawing.Point(486, 86);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(112, 21);
+			this.label3.TabIndex = 23;
+			this.label3.Text = "HOMOGÉNEA";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.Black;
+			this.label2.Location = new System.Drawing.Point(288, 59);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(110, 24);
+			this.label2.TabIndex = 22;
+			this.label2.Text = "-MEZCLAS-";
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Black;
+			this.label1.Location = new System.Drawing.Point(211, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(251, 35);
+			this.label1.TabIndex = 21;
+			this.label1.Text = "QUÍMICA III";
 			// 
 			// quimica
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "quimica";
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(684, 661);
+			this.Controls.Add(this.btnMen);
+			this.Controls.Add(this.btnHomo1);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.btnGaso);
+			this.Controls.Add(this.btnAceYagu);
+			this.Controls.Add(this.btnPerf);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.btnHetero2);
+			this.Controls.Add(this.btnHomo2);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.btnHetero1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "quimica";
+			this.Text = "quimica";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button btnHetero1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button btnHomo2;
+		private System.Windows.Forms.Button btnHetero2;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Button btnPerf;
+		private System.Windows.Forms.Button btnAceYagu;
+		private System.Windows.Forms.Button btnGaso;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Button btnHomo1;
+		private System.Windows.Forms.Button btnMen;
 	}
 }
