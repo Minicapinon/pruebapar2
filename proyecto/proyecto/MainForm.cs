2db8cced812ae70ecbd1e210ecf25400e6849853
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-31 07:28:40 -0600

    Archivos

---
+++ proyecto/proyecto/MainForm.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC02
+ * Date: 29/10/2024
+ * Time: 07:11 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyecto
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void SalirToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		
+		void InfoToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			acercade info = new acercade();
+			info.Show();
+		}
+		
+		void QuimicaIIIToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			quimica qm = new quimica();
+				qm.Show();
+		}
+		
+		void EcosistemasIIIToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			ecosistemas eco = new ecosistemas();
+			eco.Show();
+		}
+		
+		void PensamientoMatematicoIIIToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			matematicas mate = new matematicas();
+				mate.Show();
+		}
+		
+		void InglesIIIToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			ingles ing = new ingles();
+			ing.Show();
+		}
+		
+		void FisicaIIIToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			fisica fis = new fisica();
+				fis.Show();
+		}
+	}
+}
