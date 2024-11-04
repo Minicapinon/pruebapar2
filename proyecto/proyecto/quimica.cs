2f77a083de81b8701bb74f7630b9b13ba0558b4b
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-11-04 08:03:29 -0600

    Revert "Merge pull request #8 from Minicapinon/Karol"
    This reverts commit a96b4ed55e2fce9a502237df98685f52e2c49eb3, reversing
    changes made to f7c91bdbb8a9cb43b59f236d856580ddcd65df5c.

--- proyecto/proyecto/quimica.cs
+++ proyecto/proyecto/quimica.cs
@@ -28,57 +28,5 @@ namespace proyecto
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
-		
-		//RESPUESTAS 1
-		void BtnHomo1Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("¡CORRECTO! \n \n El café con leche es una mezcla homogénea " +
-			                "porque sus componentes se distribuyen uniformemente. " +
-							"No podrás ver los ingredientes separados a simple vista.");
-		}
-		void BtnHetero1Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("INCORRECTO");
-		}
-		
-		
-		//RESPUESTAS 2
-		void BtnHomo2Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("INCORRECTO");
-		}
-		void BtnHetero2Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("¡CORRECTO! \n \n El agua con arena es una mezcla heterogénea" +
-							" porque puedes ver y separar fácilmente sus componentes. " +
-							"La arena no se disuelve en el agua, creando una distinción clara.");
-		}
-		
-		
-		//RESPUESTAS 3
-		void BtnPerfClick(object sender, EventArgs e)
-		{
-			MessageBox.Show("INCORRECTO \n \n El perfume es una mezcla homogenea " +
-			                "porque sus componentes se mezclan uniformemente. " +
-			                "No puedes separar sus partes a simple vista.");
-		}
-		void BtnAceYaguClick(object sender, EventArgs e)
-		{
-			MessageBox.Show("¡CORRECTO! \n \n El agua con el aceite no son homogéneos " +
-			                "porque no se mezclan uniformemente. El aceite flota sobre el agua.");
-		}
-		void BtnGasoClick(object sender, EventArgs e)
-		{
-			MessageBox.Show("INCORRECTO \n \n La gasolina es una mezcla homogenea " +
-			                "porque sus componentes se mezclan uniformemente. " +
-			                "No puedes separar sus partes a simple vista.");
-		}
-		
-		
-		//RESGRESAR AL MENÚ
-		void BtnMenClick(object sender, EventArgs e)
-		{
-			this.Close();
-		}
 	}
 }
